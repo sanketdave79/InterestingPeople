b81ea55e3095a109817fabbe9ddbd7bd80977bf7
sanketdave79 <sanketdave79@techmentaion.com>
2014-01-29 21:03:36 +1000

    Initial Commit

---
+++ InterestingPeople/SDViewController.h
@@ -0,0 +1,13 @@
+//
+//  SDViewController.h
+//  InterestingPeople
+//
+//  Created by sanket on 29/01/2014.
+//  Copyright (c) 2014 Techmentation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDViewController : UIViewController
+
+@end
---
+++ InterestingPeople/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InterestingPeople
+//
+//  Created by sanket on 29/01/2014.
+//  Copyright (c) 2014 Techmentation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SDAppDelegate class]));
+    }
+}
